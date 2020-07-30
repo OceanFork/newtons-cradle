4c6d19f7e1b05dc6ee28e713b8a630e48cc5cbc6
OceanFork <66967337+OceanFork@users.noreply.github.com>
2020-07-30 12:50:10 -0400

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,36 @@
+class bob
+{
+	constructor(x,y,r)
+	
+	{
+		
+		var options={ 
+			isStatic:false,
+			 restitution:1, 
+			 friction:5, 
+			 density:8 }
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+		
+	
+
+	}
+	display()
+	{
+			
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+		
+			strokeWeight(0);
+			stroke("yellow");
+			fill(255);
+			ellipse(0,0,this.r, this.r);
+			pop()	
+	}
+
+}
